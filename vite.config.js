90bfd92d813f9ee957c6d4c501a142b1fdc3cf27
Maicon Gabriel Alves <47224472+maikeg-alves@users.noreply.github.com>
2022-07-01 11:45:43 -0300

    initial deploy

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
-
-/* export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "192.168.0.102",
-    port: 3000,
-  }
-})
- */
