d44bc30e18a8fc4581320e4a5c66b89bd5f55c23
Maicon Gabriel Alves <47224472+maikeg-alves@users.noreply.github.com>
2022-10-05 19:43:45 -0300

    fix bug: container for pages skills and projects

--- components/avatar/avatar.components.tsx
+++ components/avatar/avatar.components.tsx
@@ -23,8 +23,8 @@ const Avatar: NextPage<Mobile> = ({ mobile }) => {
       <Image
         src={`https://i.imgur.com/nBtsD1X.png`}
         alt={'foto de perfil'}
-        width={'300x'}
-        height={'300px'}
+        width={'500px'}
+        height={'500px'}
         priority
       />
     </MyAvatar>
